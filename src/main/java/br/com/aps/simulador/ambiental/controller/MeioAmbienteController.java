a4902eba0386a3c8e3657f8c2a86308d1fd3116a
Andre Esposito <andrezinhoesposito33@gmail.com>
2024-04-19 20:10:07 -0300

    trocando path variable

--- src/main/java/br/com/aps/simulador/ambiental/controller/MeioAmbienteController.java
+++ src/main/java/br/com/aps/simulador/ambiental/controller/MeioAmbienteController.java
@@ -12,21 +12,21 @@ import org.springframework.web.bind.annotation.*;
 public class MeioAmbienteController {
     private MeioTransporte meioTransporte;
 
-    @PostMapping("/{id}/meio-transporte")
-    public ResponseEntity<MeioTransporte> salvarInformacoesMeioTransporte(@PathVariable("id") Long id,
+    @PostMapping("/{cnh}/meio-transporte")
+    public ResponseEntity<MeioTransporte> salvarInformacoesMeioTransporte(@PathVariable("cnh") Long cnh,
                                                                           @RequestBody MeioTransporte request) {
         // Implementar a lógica para salvar as informações do meio de transporte no banco e retornar elas.
         return ResponseEntity.status(HttpStatus.CREATED).body(informacoesSalvas).build();
     }
 
-    @GetMapping("/{id}/impactos-ambientais")
-    public ResponseEntity<MeioTransporte> calcularImpactoMeioTransporte(@PathVariable("id") Long id) {
+    @GetMapping("/{cnh}/impactos-ambientais")
+    public ResponseEntity<MeioTransporte> calcularImpactoMeioTransporte(@PathVariable("cnh") Long cnh) {
         // Implementar a lógica para calcular as informações dos cálculos do impacto de co2 e retorná-los.
         return ResponseEntity.status(HttpStatus.OK).body(calculoImpactos).build();
     }
 
-    @GetMapping("/{id}/sugestoes")
-    public ResponseEntity<MeioTransporte> consultarSugestoes(@PathVariable("id") Long id) {
+    @GetMapping("/{cnh}/sugestoes")
+    public ResponseEntity<MeioTransporte> consultarSugestoes(@PathVariable("cnh") Long cnh) {
         // Implementar a lógica para sugerir como o usuário pode melhorar seu transporte para reduzir os impactos no meio ambiente e retorná-los.
         return ResponseEntity.status(HttpStatus.OK).body(sugestoes).build();
     }
